062ae1e585d9c0f6911866bbafe0ae4308a55075
WolfGang011 <thaiphong011199@gmail.com>
2024-05-17 15:24:05 +0700

    b

--- loop/lab01.java
+++ loop/lab01.java
@@ -2,10 +2,13 @@ package loop;
 
 public class lab01 {
 	public static void main(String[] args) {
+
+		// 1. Câu điều kiện for
 //		for (int i = 0; i < 5; i++) {
 //			System.out.println(i);
-//		}
+//		}ffff
 
+		// Câu điều kiện do/while
 		int j = 9;
 		do {
 			System.out.println(j);
