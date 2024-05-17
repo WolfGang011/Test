bcb25c00b61d89dd5579906f77158ccc356e4517
WolfGang011 <thaiphong011199@gmail.com>
2024-05-17 15:18:46 +0700

    test

---
+++ loop/lab01.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class lab01 {
+	public static void main(String[] args) {
+//		for (int i = 0; i < 5; i++) {
+//			System.out.println(i);
+//		}
+
+		int j = 9;
+		do {
+			System.out.println(j);
+			j++;
+		} while (j < 5);
+	}
+}
